3b009be2f66012fd5f83c9856f0aab37b271590c
exialym <exialym@icloud.com>
2016-07-03 17:57:47 +0800

    拟物化文字效果 完结 第5章 排版 完结

--- secret/common.js
+++ secret/common.js
@@ -55,5 +55,22 @@ $$('.piesvg').forEach(function(pie) {
     svg.appendChild(circle); 
     pie.appendChild(svg); 
 });
-
+$$('.circularJS').forEach(function(el) {
+    var NS = "http://www.w3.org/2000/svg";
+    var xlinkNS = "http://www.w3.org/1999/xlink";
+    var svg = document.createElementNS(NS, "svg");
+    var circle = document.createElementNS(NS, "path");
+    var text = document.createElementNS(NS, "text");
+    var textPath = document.createElementNS(NS, "textPath");
+    svg.setAttribute("viewBox", "0 0 100 100");
+    circle.setAttribute("d", "M0,50 a50,50 0 1,1 0,1z");
+    circle.setAttribute("id", "circle");
+    textPath.textContent = el.textContent;
+    textPath.setAttributeNS(xlinkNS, "xlink:href", "#circle");
+    text.appendChild(textPath);
+    svg.appendChild(circle);
+    svg.appendChild(text);
+    el.textContent = '';
+    el.appendChild(svg);
+});
 
